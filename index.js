2c387461f799587fb4e2bc0fcbe047ee98ddd49a
chicus12 <chicus12@gmail.com>
2018-02-07 14:53:31 -0600

    En esta seccion vemos como instalar paquetes en node y como crear y ejeutar scripts, ademas de instalar eslint para el control de nuestro estructura y estilo del codigo

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const { suma, resta, multiplicar } = require('./operations')
+const chalk = require('chalk');
 
-const resultados = `El resulatado de sumar: ${suma(2, 4)}`
+const { suma, resta, multiplicar } = require('./operations');
 
-console.log(resultados)
-console.log('Resultado multiplicar:', multiplicar(2, 4))
-console.log('Resultado dividir:', multiplicar(4, 2, true))
-console.log('Resultado restar:', resta(4,2))
+const resultados = `El resulatado de sumar: ${suma(2, 4)}`;
+
+console.log(resultados);
+console.log('Resultado multiplicar:', multiplicar(2, 4));
+console.log('Resultado dividir:', multiplicar(4, 2, true));
+console.log('Resultado restar:', resta(4, 2));
 
 // Object/Array destructuring assignment === extraer elementos que necesito
-const [a, b] = ['Hello', 'Jhon']
-console.log(a)
-console.log(b)
+const [a, b] = ['Hello', 'Jhon'];
+console.log(a);
+console.log(b);
 
-const { name } = { name: 'Max', age: 28 }
-console.log(name)
+const { name } = { name: 'Max', age: 28 };
+console.log(name);
 
-const { age: ageMax } = { name: 'Max', age: 28 }
-console.log(ageMax)
+const { age: ageMax } = { name: 'Max', age: 28 };
+console.log(ageMax);
+
+console.log(chalk.blue('Hello world!'));
--- operations.js
+++ operations.js
@@ -1,29 +1,21 @@
-const suma = (num1, num2) => {
-  return num1 + num2
-}
+const suma = (num1, num2) => num1 + num2;
+
+const division = (num, num2) => num / num2;
 
 const multiplicar = (num1, num2, dividir = false) => {
   if (dividir) {
-    return division(num1, num2)
+    return division(num1, num2);
   }
-  return num1 * num2
-}
+  return num1 * num2;
+};
 
-const division = (num, num2) => {
-  return num / num2
-}
+const resta = (num, num2) => num - num2;
 
-const resta = (num, num2) => {
-  return num - num2
-}
-
-const cuadrado = (num) => {
-  return num * num
-}
+const cuadrado = num => num * num;
 
 module.exports = {
   suma,
   multiplicar,
   resta,
   cuadrado,
-}
+};
